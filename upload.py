de5de9d5b6062fe803451f4ae0c1bfa74b8296ed
JuliaZur <34189609+JuliaZur@users.noreply.github.com>
2019-05-13 11:11:42 +0200

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,5 +3,5 @@ import boto3
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
 # Upload a new file
-data = open('./pictures/other/bmw.jpeg', 'rb')
+data = open('YOUR_FILE', 'rb')
 s3.Bucket('iotaghproject').put_object(Key='pic.jpg', Body=data)
